f1e86076a52b400a2cb8d59af9e1a2da327b3930
Felipe Cruz <bones.felipe@gmail.com>
2015-06-07 18:41:53 -0500

    Mejora en las regiones y caracteres, el main.m determina si es placa o no y la imprime al final

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-img = imread('fake.png');
+img = imread('crf693.jpg');
 img = preprocessing(img);
 
 %Division por regiones:
@@ -8,9 +8,8 @@ sz = size(img);
 h = sz(1);
 w = sz(2);
 areabig = h*w;
-
-
-for i = 1 : length(st)
+plate = '';
+for i = 1 : length(st);
        
       thisBB = st(i).BoundingBox;
       
@@ -22,32 +21,43 @@ for i = 1 : length(st)
       
       width = thisBB(4);
       height = thisBB(3);
-      area = abs(width*height);
+      area = (width*height);
       ratio = area/areabig;
       if abs(ratio-1) < 0.1 && length(st) > 1; continue; end %Evita evualuar toda la imagen
       % se supone que siemrpe esta el rectangulo de la placa recubriendo...
+            
+      plate = '';
+      numbers = 0;
       
-      
-      %figure, imshow(potentialPlate); hold on;
-      i
       for k = 1 : length(charBounds);
             
           charBB = charBounds(k).BoundingBox;
           char = imcrop(potentialPlate,charBB);
-          sz = size(char); h2 = sz(1); w2 = sz(2);
-          if (h2*w2)/(h*w) > 0.1274666; continue; end
-          if w2 > h2; continue; end
-          %rectangle('Position', [charBB(1),charBB(2),charBB(3),charBB(4)],'EdgeColor','r','LineWidth',2 )
-          figure, imshow(char);
-          
-          
+          h2 = charBB(3); w2 = charBB(4);
+          ratio = (h2*w2)/(area);
+          delta = abs(ratio-0.08);
+          if delta > 0.05;  continue; end %Descarta las cosas pequenas que estorban
+         
           [flag,val] = recognize(char,horzcat('A':'Z','0':'9'));
           
-          if flag ~= -1;              
-              disp(flag);
+          if val >= 0.6; 
+              plate = strcat(plate,flag);
+              if '0' <= flag && flag <= '9';
+                  numbers = numbers + 1;
+              end
+              
           end
-          
-          
       end
-      
+    
+      disp(plate);
+      if length(plate) == 6 && numbers == 3; 
+          break;
+      end
 end
+
+if length(plate) ~= 6;
+    disp('No es una placa valida j3j3j3j3j3');
+else
+    disp(strcat('La puta placa es: ',plate));
+end
+
